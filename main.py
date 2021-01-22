039211e61cc727bbadc497043849e0ca96d99740
Coder-Yorick <yorick442002@gmail.com>
2021-01-22 17:28:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ cs = CloudSheet()
 TARGETS = ['2330', '1907', '2353', '1218']
 
 def main():
-    stockids = ['2353']
+    stockids = TARGETS
     for stockid in stockids:
         stock_data = get_stock_data(stockid)
         if stock_data:
             result = my_analysis(stockid, stock_data)
-            store(stockid, result)
-            print('{}:done!'.format(stockid))
+            if store(stockid, result):
+                print('{}:done!'.format(stockid))
         else:
             print('{}:解析失敗'.format(stockid))
         # results = processdata(stockid, soupdata)
@@ -70,24 +70,67 @@ def my_analysis(stockid, soupdata):
     in_brokers = sorted(in_brokers, key=lambda bk : bk['amount'], reverse=True)
     d['買超券商'] = []
     top_in_amount = 0
-    for in_broker in in_brokers[:3]:
+    for in_broker in in_brokers[:10]:
         top_in_amount += in_broker['amount']
-        d['買超券商'].append(in_broker['name'])
-    d['買超券商'] = ','.join(d['買超券商'])
-    d['籌碼集中度'] = top_in_amount / in_amount if in_amount > 0 else None
+        d['買超券商'].append({
+            'name': in_broker['name'],
+            'amount': in_broker['amount'],
+            'price': in_broker['account'] / in_broker['amount']
+        })
+    d['當日籌碼集中度'] = top_in_amount / in_amount if in_amount > 0 else None
     return d
 
 def store(stockid, data):
     try:
         ws = cs.openSheet("twse")
+        # 每日紀錄
         sh = ws.worksheet(stockid)
-        row_count = len(sh.col_values(1))
-        row_count = 2 if row_count == 0 else row_count + 1
-        cols = ['日期', '開盤價', '最高價', '最低價', '收盤價', '總成交股數', '買超股數', '買超券商家數', '籌碼集中度']
+        dates = sh.col_values(1)
+        row_count = 2
+        if len(dates) > 1:
+            row_count = len(dates) + 1
+            last_update_date = datetime.strptime(dates[-1], '%Y-%M-%d').date()
+            if last_update_date == data['日期']:
+                print('{}:資料已存在({})'.format(stockid, str(last_update_date)))
+                return False
+        cols = ['日期', '開盤價', '最高價', '最低價', '收盤價', '總成交股數', '買超股數', '買超券商家數', '當日籌碼集中度']
         for i, col in enumerate(cols):
             sh.update_cell(row_count, i + 1, str(data[col]))
+        # 籌碼紀錄
+        ho_sh = ws.worksheet('holdings')
+        ho_stocks = ho_sh.row_values(1)
+        for i, ho_stock in enumerate(ho_stocks):
+            if ho_stock == stockid:
+                broker_datas = ho_sh.col_values(i+1)[1:]
+                brokers = {}
+                for broker_data in broker_datas:
+                    b = broker_data.split('$$')
+                    if len(b) == 2:
+                        brokers[b[0]] = int(b[1])
+                for c_broker in data['買超券商']:
+                    if c_broker['name'] not in brokers:
+                        brokers[c_broker['name']] = c_broker['amount']
+                    else:
+                        brokers[c_broker['name']] += c_broker['amount']
+                holding_brokers = sorted(brokers.items(), key=lambda bk : bk[1], reverse=True)
+                if len(holding_brokers) > 10:
+                    holding_brokers = holding_brokers[:10]
+                # clean
+                [ho_sh.update_cell(x + 2, i + 1, '') for x in range(len(broker_datas))]
+                # record
+                holding_total_amount = 0
+                for y, (holding_broker_name, holding_broker_amount) in enumerate(holding_brokers):
+                    ho_sh.update_cell(y + 2, i + 1, '{}$${}'.format(holding_broker_name, holding_broker_amount))
+                    holding_total_amount += holding_broker_amount
+                # 累計籌碼集中度
+                in_amounts = sh.col_values(7)[1:]
+                holding = holding_total_amount / sum([int(in_amount.replace(',', '')) for in_amount in in_amounts])
+                sh.update_cell(row_count, 12, holding)
+                break
+        return True
     except Exception as ex:
         print(ex)
+    return False
 
 def get_stock_data(stockid):
     result = 2
